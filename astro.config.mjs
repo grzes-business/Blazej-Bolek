cef9fc4837a437d297e5024894ad179fbe3d81b7
Grzegorz Diaconescu <grzes.diac@gmail.com>
2024-10-15 10:10:58 +0200

    Deployed to the web using GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://grzes-business.github.io',
+  base: 'my-repo'
 });
